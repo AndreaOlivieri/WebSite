e6c4484d295d61630ceba098a1f5763c3bb31ae3
Andrea Olivieri <andrea.olivieri1991.gmail.com>
2015-08-10 15:24:23 +0200

    add PHP mailer

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -13,28 +13,23 @@
     $subject = "Website Email";
 
     //create an instance of PHPMailer
-    $mail = new PHPMailer(true);
-
-    try {
-      $mail->From = $email;
-      $mail->FromName = $name;
-      $mail->AddAddress($to); //recipient 
-      $mail->Subject = $subject;
-      $mail->Body = $message;
-      $mail->Send();
-      echo "Message Sent OK\n";
-    } catch (phpmailerException $e) {
-      echo $e->errorMessage(); //Pretty error messages from PHPMailer
-    } catch (Exception $e) {
-      echo $e->getMessage(); //Boring error messages from anything else!
-    }
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
 
-      if($mail->Send()) {
-          echo "success"; // this method sends the mail.
-      }else{
-          echo "error: something wrong"; // this method sends the mail.
+      $mail = new PHPMailer(true);
+
+      try {
+        $mail->From = $email;
+        $mail->FromName = $name;
+        $mail->AddAddress($to); //recipient 
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->Send();
+        echo "success";
+      } catch (phpmailerException $e) {
+        echo $e->errorMessage(); //Pretty error messages from PHPMailer
+      } catch (Exception $e) {
+        echo $e->getMessage(); //Boring error messages from anything else!
       }
     }else{
         echo "error: Invalid Email"; //error
