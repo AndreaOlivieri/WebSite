be1ecedbfc150d4e23089940364dc4c816013e43
Andrea Olivieri <andrea.olivieri1991.gmail.com>
2015-08-10 13:16:02 +0200

    prova 5 email

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,30 +1,15 @@
 <?php
+$to      = 'andrea.olivieri1991@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: oli.andrea17@gmail.com' . "\r\n" .
+    'Reply-To: oli.andrea17@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-    // Here we get all the information from the fields sent over by the form.
-    $name = $_REQUEST['user-name'];
-    $email = $_REQUEST['user-email'];
-    $message = ( $_REQUEST['user-message'] ) ? $_REQUEST['user-message'] : '';
-    $status = $_REQUEST['user-status'];
-
-    $to = 'andrea.olivieri1991@gmail.com';
-    $subject = "Website Email";
-
-    $headers = "From: ".$name." <".$email.">". "\r\n" .
-               "Reply-To: ".$name." <".$email.">". "\r\n".
-               "MIME-Version: 1.0\r\n".
-               "Content-type: text/html; charset=iso-8859-1\r\n".
-               "X-Priority: 1\r\n".
-               "X-Mailer: PHP/".phpversion();
-
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
-        if (mail($to, $subject, $message, $headers)) { // this method sends the mail.
+if (mail($to, $subject, $message, $headers)) { // this method sends the mail.
           echo "success"; // success message
         }
         else{
           echo "error"; //error
         }
-    }else{
-        echo "error"; //error
-    }
-
 ?>
