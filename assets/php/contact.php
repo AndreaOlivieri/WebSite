f4a36f0c03e22ee6407a7e3c51fec0bfd09dc908
Andrea Olivieri <andrea.olivieri1991.gmail.com>
2015-08-10 12:43:48 +0200

    prova 4 email

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -5,7 +5,7 @@ $to      = 'andrea.olivieri1991@gmail.com';
 $subject = 'the subject';
 $message = 'hello';
 $headers = 'From: andrea.olivieri1991@gmail.com' . "\r\n" .
-    'Reply-To: andrea.olivieri1991@gmail.com.com' . "\r\n" .
+    'Reply-To: andrea.olivieri1991@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
