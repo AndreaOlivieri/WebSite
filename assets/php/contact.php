5243bfcd1f4dbc390ca168a780f163a6e3d6a955
Andrea Olivieri <andrea.olivieri1991.gmail.com>
2015-08-10 14:09:24 +0200

    prova 5 email

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -7,14 +7,17 @@
     $status = $_REQUEST['user-status'];
 
     $to = 'andrea.olivieri1991@gmail.com';
-    $subject = "Website Email";
+    $message = 'Name: '.$name.'<br /> Email: '.$email.'<br />Message: '.$message;
+
+    $subject = 'Subject';
 
     $headers = "From: ".$name." <".$email.">". "\r\n" .
-               "Reply-To: ".$name." <".$email.">". "\r\n".
+               "Return-Path: Andrea Olivieri <".$to.">\r\n".
+               "Reply-To: Andrea Olivieri <".$to.">" . "\r\n" .
                "MIME-Version: 1.0\r\n".
                "Content-type: text/html; charset=iso-8859-1\r\n".
-               "X-Priority: 1\r\n".
-               "X-Mailer: PHP/".phpversion();
+               "X-Priority: 3\r\n" .
+               'X-Mailer: PHP/' . phpversion();
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
         if (mail($to, $subject, $message, $headers)) { // this method sends the mail.
