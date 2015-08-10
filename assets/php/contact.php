1c0639a3a8ac07c9cb89185c5c8c0dde1d584cf7
Andrea Olivieri <andrea.olivieri1991.gmail.com>
2015-08-10 15:42:42 +0200

    nothing

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,40 +1,29 @@
 <?php
-    require_once 'PHPMailer/PHPMailerAutoload.php';
-
 
     // Here we get all the information from the fields sent over by the form.
-    $name = $_REQUEST["user-name"];
-    $email = $_REQUEST["user-email"];
-    $message = ( $_REQUEST["user-message"] ) ? $_REQUEST["user-message"] : "";
-    $status = $_REQUEST["user-status"];
+    $name = $_REQUEST['user-name'];
+    $email = $_REQUEST['user-email'];
+    $message = ( $_REQUEST['user-message'] ) ? $_REQUEST['user-message'] : '';
+    $status = $_REQUEST['user-status'];
 
-    $to = "andrea.olivieri1991@gmail.com";
+    $to = 'andrea.olivieri1991@gmail.com';
+    $message = 'Name: '.$name.'<br /> Email: '.$email.'<br />Message: '.$message;
 
-    $subject = "Website Email";
+    $subject = 'Email from Website';
 
-    //create an instance of PHPMailer
+    $headers = "From: ".$name." <".$email.">". "\r\n" .
+               "Reply-To: Andrea Olivieri <".$to.">" . "\r\n" .
+               "MIME-Version: 1.0\r\n".
+               "Content-type: text/html; charset=iso-8859-1\r\n".
+               "X-Priority: 3\r\n" .
+               "X-Mailer: PHP/" . phpversion();
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
-
-      $mail = new PHPMailer(true);
-      
-      try {
-        $mail->IsSMTP();
-        $mail->Host = "smtp.gmail.com";
-        $mail->From = $email;
-        $mail->FromName = $name;
-        $mail->AddAddress($to); //recipient 
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-        $mail->Send();
-        echo "success";
-      } catch (phpmailerException $e) {
-        echo $e->errorMessage(); //Pretty error messages from PHPMailer
-      } catch (Exception $e) {
-        echo $e->getMessage(); //Boring error messages from anything else!
-      }
+        mail($to, $subject, $message, $headers); // this method sends the mail.
+        echo "success"; // success message
+        exit;
     }else{
-        echo "error: Invalid Email"; //error
+        echo "error"; //error
     }
 
 ?>
