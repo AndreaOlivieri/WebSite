6f0603b46574f848b60bf99cb70a29d5bf3e9844
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-05-25 17:31:08 +0200

    fine

--- Gemfile
+++ Gemfile
@@ -46,14 +46,14 @@ group :development do
   gem 'capistrano-rails', require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano-rvm'
+
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
 end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_template :home
+    assert_template layout: "layouts/application"
+    assert_select 'title', "Home | Andrea Olivieri"
+  end
+
+  test "should get profile" do
+    get :profile
+    assert_response :success
+    assert_template :profile
+    assert_template layout: "layouts/application"
+    assert_select 'title', "Profile | Andrea Olivieri"
+  end
+
+  test "should get resume" do
+    get :resume
+    assert_response :success
+    assert_template :resume
+    assert_template layout: "layouts/application"
+    assert_select 'title', "Resume | Andrea Olivieri"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_template :contact
+    assert_template layout: "layouts/application"
+    assert_select 'title', "Contact | Andrea Olivieri"
+  end
 end
